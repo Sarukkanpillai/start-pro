e5a2b6864c5d3146e9b631d0d98c227fd4d3937f
Sarukkanpillai <sarukkanpillai95@gmail.com>
2022-03-29 22:22:42 +0530

    pro

---
+++ src/com/emp/TamilMovies.java
@@ -0,0 +1,24 @@
+package com.emp;
+
+public class TamilMovies {
+	
+	public void kakkaMutta()
+	{
+		System.out.println(" vignesh");
+	}
+	public void valimai()
+	{
+		System.out.println(" ajith kumar");
+		System.out.println("sarukkan");
+	}
+	public static void main(String[] args) {
+		
+		TamilMovies a = new TamilMovies();
+		a.kakkaMutta();
+		a.valimai();
+		a.kakkaMutta();
+		a.kakkaMutta();
+		a.kakkaMutta();
+	}
+
+}
---
+++ src/com/fram/Framecls.java
@@ -0,0 +1,5 @@
+package com.fram;
+
+public class Framecls {
+
+}
